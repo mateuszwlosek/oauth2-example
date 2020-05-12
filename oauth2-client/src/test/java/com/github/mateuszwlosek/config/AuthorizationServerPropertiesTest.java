b39b0940133a6307f2608d704d5f87c1d24ebd4e
Mateusz Wlosek <frast@windowslive.com>
2020-05-12 14:45:37 -0500

    circle config change

---
+++ oauth2-client/src/test/java/com/github/mateuszwlosek/config/AuthorizationServerPropertiesTest.java
@@ -0,0 +1,13 @@
+package com.github.mateuszwlosek.config;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+class AuthorizationServerPropertiesTest {
+
+    @Test
+    public void test() {
+        Assert.assertTrue(false);
+    }
+}
